925c21d2e2680a81c1fab673389ab4b956c37f1a
Fauzi Rizal <elachim26@gmail.com>
2019-07-08 14:31:21 +0700

    Initial Commit

---
+++ vlidea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vlidea
+//
+//  Created by Fauzi Rizal on 08/07/19.
+//  Copyright © 2019 Fauzi Rizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
